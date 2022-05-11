b2fb0ac718edf6134e398c4cebdca053cd17fe59
legolai <nico360f@gmail.com>
2022-05-11 09:32:32 +0200

    rebase origin/main and add Carport algo test file

---
+++ src/test/java/dk/cphbusiness/dat/carporteksamensproject/model/services/CarportAlgorithmTest.java
@@ -0,0 +1,16 @@
+package dk.cphbusiness.dat.carporteksamensproject.model.services;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CarportAlgorithmTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+
+
+
+}
